88ee7ac6df6cfa68bb56a4b38df52675afb1e211
marcolagos <mal18@rice.edu>
2022-03-19 16:31:08 -0500

    example markdown with styling

--- src/Components/References/ReferencesMarkdown/Media/Media.js
+++ src/Components/References/ReferencesMarkdown/Media/Media.js
@@ -3,7 +3,7 @@ import React from "react";
 function Media({ folder, image }) {
 	const imagePath = require(`../../../../Assets/References/${folder}/${image}`);
 	return (
-		<div className="image-container" style={{height: 200, width: 500, display: "block"}}>
+		<div className="image-container" align="center">
 			<img src={imagePath} className="image" alt="image"/>
 		</div>
 	);
